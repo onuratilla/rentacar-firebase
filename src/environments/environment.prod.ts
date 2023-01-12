aed044cf0d86d40e5a4c8dc01bd9dc763b887668
Onur Atilla <118292337+onuratilla@users.noreply.github.com>
2023-01-12 22:16:38 +0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'onuratilla-d4bf7',
+    appId: '1:597540392563:web:79580c16e2aa7a63942406',
+    storageBucket: 'onuratilla-d4bf7.appspot.com',
+    apiKey: 'AIzaSyDEfplqapUndGnU9AkVellV53nghTeQ0jU',
+    authDomain: 'onuratilla-d4bf7.firebaseapp.com',
+    messagingSenderId: '597540392563',
+    measurementId: 'G-5K8KW6RFC8',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'onuratilla-d4bf7',
+    appId: '1:597540392563:web:79580c16e2aa7a63942406',
+    storageBucket: 'onuratilla-d4bf7.appspot.com',
+    apiKey: 'AIzaSyDEfplqapUndGnU9AkVellV53nghTeQ0jU',
+    authDomain: 'onuratilla-d4bf7.firebaseapp.com',
+    messagingSenderId: '597540392563',
+    measurementId: 'G-5K8KW6RFC8',
+  },
+  production: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
